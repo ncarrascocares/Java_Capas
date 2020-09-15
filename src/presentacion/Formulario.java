1efd4750d7b6eee32472dd7e8623ccf970dd3258
nicolascarrascocares <53540839+nicolascarrascocares@users.noreply.github.com>
2020-09-15 10:05:31 -0400

    decimo primer commit, preparacion del boton agregar en el Formulario

--- src/presentacion/Formulario.java
+++ src/presentacion/Formulario.java
@@ -5,17 +5,26 @@
  */
 package presentacion;
 
+import datos.DBCliente;
+import javax.swing.table.DefaultTableModel;
+import negocio.Cliente;
+
+
 /**
  *
  * @author ncarrasco
  */
 public class Formulario extends javax.swing.JFrame {
-
-    /**
-     * Creates new form Formulario
-     */
+    
+    private DefaultTableModel modelo;
+    private DBCliente dbcl;
+    
     public Formulario() {
         initComponents();
+        dbcl = new DBCliente();
+        modelo = dbcl.lista_grilla();
+        this.jTable1.setModel(modelo);
+        
     }
 
     /**
@@ -27,21 +36,157 @@ public class Formulario extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        id_txt = new javax.swing.JTextField();
+        nombre_txt = new javax.swing.JTextField();
+        direccion_txt = new javax.swing.JTextField();
+        saldo_txt = new javax.swing.JTextField();
+        agregar_btn = new javax.swing.JButton();
+        modificar_btn = new javax.swing.JButton();
+        eliminar_btn = new javax.swing.JButton();
+        limpiar_btn = new javax.swing.JButton();
+        buscar_btn = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("ID");
+
+        jLabel2.setText("Nombre:");
+
+        jLabel3.setText("Dirección:");
+
+        jLabel4.setText("Saldo:");
+
+        agregar_btn.setText("Agregar");
+        agregar_btn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                agregar_btnActionPerformed(evt);
+            }
+        });
+
+        modificar_btn.setText("Modificar");
+
+        eliminar_btn.setText("Eliminar");
+
+        limpiar_btn.setText("Limpiar");
+
+        buscar_btn.setText("Buscar");
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "ID", "Nombre", "Direccion", "Saldo"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(agregar_btn)
+                        .addGap(18, 18, 18)
+                        .addComponent(modificar_btn)
+                        .addGap(18, 18, 18)
+                        .addComponent(eliminar_btn))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel1))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(id_txt, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(28, 28, 28)
+                                .addComponent(buscar_btn))
+                            .addComponent(nombre_txt, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel4))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(saldo_txt, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(limpiar_btn))
+                            .addComponent(direccion_txt, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(12, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(id_txt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(buscar_btn))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(nombre_txt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(direccion_txt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel4)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(saldo_txt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(limpiar_btn)))
+                        .addGap(42, 42, 42)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(agregar_btn)
+                            .addComponent(modificar_btn)
+                            .addComponent(eliminar_btn))))
+                .addContainerGap(11, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void agregar_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_agregar_btnActionPerformed
+        dbcl = new DBCliente();
+        Cliente clte = new Cliente();
+        String id, nombre, direccion, saldo;
+        int _id;
+        float _saldo;
+        id = this.id_txt.getText();
+        nombre = this.nombre_txt.getText();
+        direccion = this.direccion_txt.getText();
+        saldo = this.saldo_txt.getText();
+        
+        _id = Integer.parseInt(id);
+        _saldo = Float.parseFloat(saldo);
+        clte.setId(_id);
+        clte.setNombre(nombre);
+        clte.setDireccion(direccion);
+        clte.setSaldo(_saldo);
+        dbcl.agregar_cliente(clte);
+        modelo = dbcl.lista_grilla();
+        this.jTable1.setModel(modelo);
+        
+    }//GEN-LAST:event_agregar_btnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +224,20 @@ public class Formulario extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton agregar_btn;
+    private javax.swing.JButton buscar_btn;
+    private javax.swing.JTextField direccion_txt;
+    private javax.swing.JButton eliminar_btn;
+    private javax.swing.JTextField id_txt;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JButton limpiar_btn;
+    private javax.swing.JButton modificar_btn;
+    private javax.swing.JTextField nombre_txt;
+    private javax.swing.JTextField saldo_txt;
     // End of variables declaration//GEN-END:variables
 }
