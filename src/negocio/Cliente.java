eebc92c17342e1ac9d6b0684503c37402ab26f89
nicolascarrascocares <53540839+nicolascarrascocares@users.noreply.github.com>
2020-09-15 07:40:43 -0400

    septimo commit, creacion de los atributos, constructores y metodos get y set en la clase Cliente.

--- src/negocio/Cliente.java
+++ src/negocio/Cliente.java
@@ -4,4 +4,59 @@ package negocio;
 
 public class Cliente {
     
+    //atributos de la clase
+    private int id;
+    private String nombre;
+    private String direccion;
+    private float saldo;
+    
+    //Constructor sin parametros
+    public Cliente() {
+    }
+    
+    //Constuctor con parametros
+    public Cliente(int id, String nombre, String direccion, float saldo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.saldo = saldo;
+    }
+    
+    //Métedos get y set
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    
+    
+    
+    
 }
